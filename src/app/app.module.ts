8d1cc14e38de3e9fa6319e9518404040888fb663
Neduj3819 <v.mahovka@sos-credit.com.ua>
2021-05-05 12:54:06 +0300

    translate core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {MaterialModule} from './material/material.module';
+import { ToolbarComponent } from './navigation/toolbar/toolbar.component';
 
 
 export function createTranslateLoader(http: HttpClient): TranslateHttpLoader {
@@ -14,11 +16,14 @@ export function createTranslateLoader(http: HttpClient): TranslateHttpLoader {
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+    MaterialModule,
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
+const MAT_COMPONENTS = [
+  MatToolbarModule,
+  MatButtonModule,
+];
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  exports: [MAT_COMPONENTS],
+  imports: [MAT_COMPONENTS]
 })
 export class MaterialModule { }
